a88f74ed48dfef2cb557a3dbbd1a1e8b02a57245
robertvoss <vossrobertt@gmail.com>
2022-12-21 20:55:31 -0300

    commit dando erro nos treinadores

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import "./Formulario.css"
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import { useState } from 'react'
 
 const Formulario = () => {
 
@@ -15,19 +16,47 @@ const Formulario = () => {
         'Naruto'
     ]
 
-    const aoSalvar = (evento) => {
+    const [nome, setNome] = useState('')
+    const [tipo, setTipo] = useState('')
+    const [imagem, setImagem] = useState('')
 
+    const aoSalvar = (evento) => {
         evento.preventDefault()
+        console.log('Form foi submetido => ', nome, tipo, imagem)
     }
 
     return (
         <section className="formulario">
             <form onSubmit={() => aoSalvar()}>
                 <h2>Preencha os dados para criar o PokéCard</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome do Pokemon" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu tipo" />
-                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereco da imagem" />
-                <ListaSuspensa obrigatorio={true} label="Treinador" itens={treinadores} />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite o nome do Pokemon"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Tipo"
+                    placeholder="Digite seu tipo"
+                    valor={tipo}
+                    aoAlterado={valor => setTipo(valor)}
+
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Imagem"
+                    placeholder="Digite o endereco da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Treinador"
+                    itens={treinadores} 
+                    valor={treinador}
+                    aoAlterado={valor => setTreinador(valor)/>
                 <Botao>
                     Criar Card
                 </Botao>
